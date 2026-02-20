d10ead6ba2e9c893b8fca82201806c2d6d3df2d3
Favio Gabriel Munayco Rivera <75335038+JebGy@users.noreply.github.com>
2026-02-20 09:00:41 -0500

    Remove duplicate items from DEFAULT_ITEMS
    Removed duplicate items from the DEFAULT_ITEMS array.

--- public/js/ruleta.js
+++ public/js/ruleta.js
@@ -1,10 +1,10 @@
 const DEFAULT_ITEMS = [
+  { label: "Llavero 🔑", weight: 2 },
+  { label: "Libreta 📒", weight: 2 },
   { label: "Shaker 🥤", weight: 2 },
+  { label: "Bolsa 👜", weight: 2 },
   { label: "Vuelve a girar 🎡", weight: 3 },
   { label: "Cuaderno 📓", weight: 2 },
-  { label: "Bolsa 👜", weight: 2 },
-  { label: "Llavero 🔑", weight: 2 },
-  { label: "Bolsa 👜", weight: 2 },
   { label: "Llavero 🔑", weight: 2 },
   { label: "Vuelve a girar 🎡", weight: 3 },
   { label: "Perdiste 😭", weight: 3 },
